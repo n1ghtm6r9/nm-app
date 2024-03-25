f485a38aa24493bb1259fee76d07420e974f5775
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-03-25 21:17:26 +0200

    feat(): update gql exception filter

--- src/GqlExceptionFilter.ts
+++ src/GqlExceptionFilter.ts
@@ -1,9 +1,14 @@
 import { GraphQLError } from 'graphql';
 import { ExceptionFilter, Catch } from '@nestjs/common';
+import { AlreadyExistError } from '@nmxjs/errors';
 
 @Catch()
 export class GqlExceptionFilter implements ExceptionFilter {
   public catch(error) {
+    if (error.code === '23505') {
+      error = new AlreadyExistError();
+    }
+
     throw new GraphQLError(error.message, {
       extensions: {
         code: error.code,

9fd71bf697cf75fe54ba3da80f34f2444321936f
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-10-10 19:55:43 +0300

    feat(): add 404 error

--- src/GqlExceptionFilter.ts
+++ src/GqlExceptionFilter.ts
@@ -1,12 +1,18 @@
 import { GraphQLError } from 'graphql';
-import { ExceptionFilter, Catch } from '@nestjs/common';
+import { ExceptionFilter, Catch, NotFoundException } from '@nestjs/common';
 import { AlreadyExistError } from '@nmxjs/errors';
 
 @Catch()
 export class GqlExceptionFilter implements ExceptionFilter {
-  public catch(error) {
+  public catch(error, host) {
     if (error.code === '23505') {
       error = new AlreadyExistError();
+    }
+
+    if (error instanceof NotFoundException) {
+      const ctx = host.switchToHttp();
+      const res = ctx.getResponse();
+      return res.status(404).json({ message: { statusCode: 404, error: 'Not Found', message: error.message } });
     }
 
     throw new GraphQLError(error.message, {

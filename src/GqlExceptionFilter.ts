5a9a5ca08c3c6875976fb439b0d085de9649a879
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2025-06-11 17:30:01 +0300

    feat(): update

--- src/GqlExceptionFilter.ts
+++ src/GqlExceptionFilter.ts
@@ -8,10 +8,6 @@ export class GqlExceptionFilter implements ExceptionFilter {
   constructor(private readonly notifier?: INotifier) {}
 
   public catch(error, host) {
-    if (this.notifier) {
-      this.notifier.sendError(error);
-    }
-
     if (error.code === '23505') {
       error = new AlreadyExistError();
     }
@@ -20,6 +16,10 @@ export class GqlExceptionFilter implements ExceptionFilter {
       const ctx = host.switchToHttp();
       const res = ctx.getResponse();
       return res.status(404).json({ message: { statusCode: 404, error: 'Not Found', message: error.message } });
+    }
+
+    if (this.notifier && !error.silent) {
+      this.notifier.sendError(error);
     }
 
     throw new GraphQLError(error.message, {
